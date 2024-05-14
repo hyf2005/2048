6c21712af1a6c54ef3c9cfdd43c9c8b9955c2c27
geewon1i <948756589@qq.com>
2024-05-14 21:11:57 +0800

    Update LoginFrame.java

--- src/view/LoginFrame.java
+++ src/view/LoginFrame.java
@@ -2,6 +2,11 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class LoginFrame extends JFrame {
@@ -30,20 +35,44 @@ public class LoginFrame extends JFrame {
         passwordText.setSize(100, 100);
         this.getContentPane().add(passwordText);
 
-        JTextField password = new JTextField();
+
+        JPasswordField password = new JPasswordField();
         password.setLocation(280, 185);
         password.setSize(250, 30);
         this.getContentPane().add(password);
 
+        //登录
         JButton login = new JButton("登录");
         login.setLocation(180, 300);
-        login.setSize(100,50);
+        login.setSize(100, 50);
         this.getContentPane().add(login);
+        login.addActionListener(e -> {
+            System.out.println("login");
+            String usernamestr = username.getText();
+            String passwordstr = password.getText();
+            login(usernamestr, passwordstr);
+        });
 
+        //注册
         JButton register = new JButton("注册");
         register.setLocation(330, 300);
-        register.setSize(100,50);
+        register.setSize(100, 50);
         this.getContentPane().add(register);
+        register.addActionListener(e -> {
+            System.out.println("register");
+            register();
+        });
+
+
+        JButton guestLogin = new JButton("游客登录");
+        guestLogin.setLocation(430,400);
+        guestLogin.setSize(120,30);
+        this.getContentPane().add(guestLogin);
+        guestLogin.addActionListener(e -> {
+            this.dispose();
+            GameFrame gameFrame = new GameFrame(700, 500);
+            gameFrame.setVisible(true);
+        });
 
     }
 
@@ -55,4 +84,56 @@ public class LoginFrame extends JFrame {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setAlwaysOnTop(true);
     }
+
+
+    private void login(String usernamestr, String passwordstr) {
+        File file = new File("C:\\Users\\94875\\IdeaProjects\\untitled4\\src\\userinfo.txt");
+        ArrayList<String> list = readString(file);
+        System.out.println(list);
+        if (usernamestr.isEmpty() ){
+            JOptionPane.showMessageDialog(this, "请输入用户名", "用户名", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        if (passwordstr.isEmpty() ){
+            JOptionPane.showMessageDialog(this, "请输入密码" , "密码", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+            for (int i = 0; i < list.size(); i++) {
+                String[] strings = list.get(i).split("&");
+                if (strings[0].equals(usernamestr) && strings[1].equals(passwordstr)) {
+                    this.dispose();
+                    GameFrame gameFrame = new GameFrame(700, 500);
+                    gameFrame.setVisible(true);
+                    return;
+                }
+            }
+        JOptionPane.showMessageDialog(this, "用户名或密码错误", "用户名或密码错误", JOptionPane.ERROR_MESSAGE);
+
+
+    }
+
+    private void register() {
+        this.dispose();
+        RegisterFrame registerFrame = new RegisterFrame();
+        registerFrame.setVisible(true);
+
+    }
+
+    public static ArrayList<String> readString(File file) {
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            // 构造一个BufferedReader类来读取文件
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String s = null;
+            // 使用readLine方法，一次读一行
+            while ((s = br.readLine()) != null) {
+                list.add(s);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
+
